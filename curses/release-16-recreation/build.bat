4d9bb27457c4c8a13ce85653327f910ac5bc25d1
Christopher Drum <development@christopherdrum.com>
2025-01-29 10:03:56 +0900

    redirect curses.z8 build into primary build folder

--- curses/release-16-recreation/build.bat
+++ curses/release-16-recreation/build.bat
@@ -1,3 +1,3 @@
 @echo off
 
-inform6 ++"D:\_Development\interactive fiction\z_machine\tools" $$huge -v8 -xr curses.inf curses.z8
+inform6 ++"D:\_Development\interactive fiction\z_machine\tools" $$huge -v8 -xr curses.inf ..\..\build\curses.z8
